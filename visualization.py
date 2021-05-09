160268066b0ed33770f89d69e6087287f0472b05
mkoncek <marian.koncek@mailbox.org>
2021-05-09 21:44:02 +0200

    Add grouping for visualization

--- visualization.py
+++ visualization.py
@@ -19,34 +19,81 @@ colors = {
 with open("results.json", "r") as results_file:
     results = json.load(results_file)
 
-width = math.ceil(math.sqrt(len(results)))
-fig, axes = plt.subplots(
-    ncols = width,
-    nrows = (len(results) + width - 1) // width,
-    # This determines the size in pixels of the final image
-    figsize = [width * 6 * i for i in (1, 1)]
-)
+def normalize(description, names):
+    columns = {}
+    result = {
+        "description": description,
+        "columns": columns,
+    }
+    for name in names:
+        maximum = 0
+        for column_values in results[name]["columns"].values():
+            for value in column_values:
+                maximum = max(maximum, value)
+        for column_name, column_values in results[name]["columns"].items():
+            new_column = columns.setdefault(column_name, [])
+            for value in column_values:
+                new_column.append(value / maximum)
+    return result
 
-for result, index in zip(results.values(), range(len(results))):
-    ax = axes[index // width][index % width]
-    labels = list(result["columns"].keys())
-    data = [column for column in result["columns"].values()]
-    
-    bar = ax.bar(
-        labels,
-        [numpy.average(d) * 1000 for d in data],
-        yerr = [numpy.std(d) * 1000 for d in data],
-        width = 0.35,
-        color = [colors[col] for col in labels],
+def savefigs(output_name, results):
+    width = math.ceil(math.sqrt(len(results)))
+    fig, axes = plt.subplots(
+        ncols = width,
+        nrows = (len(results) + width - 1) // width,
+        # This determines the size in pixels of the final image
+        figsize = [width * 6 * i for i in (1, 1)]
     )
     
-    ax.set_title(result["description"])
-    
-    ax.set_ylim(0)
-    
-    ## Set axis names
-    ax.yaxis.grid(True)
-    ax.set_xlabel("Database type")
-    ax.set_ylabel("Time [ms]")
+    for result, index in zip(results, range(len(results))):
+        ax = axes[index // width][index % width]
+        labels = list(result["columns"].keys())
+        data = [column for column in result["columns"].values()]
+        
+        bar = ax.bar(
+            labels,
+            [numpy.average(d) * 1000 for d in data],
+            yerr = [numpy.std(d) * 1000 for d in data],
+            width = 0.35,
+            color = [colors[col] for col in labels],
+        )
+        
+        ax.set_title(result["description"])
+        
+        ax.set_ylim(0)
+        
+        ## Set axis names
+        ax.yaxis.grid(True)
+        ax.set_xlabel("Database type")
+        ax.set_ylabel("Time [ms]")
+        
+    plt.savefig(output_name)
 
-plt.savefig("queries.png")
+savefigs("queries-grouped.png", [normalize("selecty", [
+        "6", "7", "21", "24", "26", "28",
+    ]),
+    normalize("selecty s podmienkami", [
+        "1", "2", "3", "4", "5", "29", "31", "32",
+    ]),
+    normalize("join", [
+        "8",
+    ]),
+    normalize("agregacie", [
+        "9", "10", "11", "12", "14", "23", "30",
+    ]),
+    normalize("select + podmienka + agregace", [
+        "13"
+    ]),
+    normalize("doplnkovy count na prikazoch", [
+        "15", "16", "17", "18", "19", "20", "22", "25", "27",
+    ]),
+    normalize("insert jeden zaznam", [
+        "33",
+    ]),
+    normalize("update", [
+        "34", "35", "36", "37", "38", "39", "40",
+    ]),
+    normalize("delete", [
+        "41", "42", "43", "44", "45",
+    ]),
+])
