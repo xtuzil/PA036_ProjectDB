63c56a576ccf7b6f29d619b32964ba214c791d8b
mkoncek <marian.koncek@mailbox.org>
2021-05-12 09:59:55 +0200

    Don't overlap labels, create one picture per query too

--- visualization.py
+++ visualization.py
@@ -10,10 +10,18 @@ colors = {
     "MongoDB": "limegreen",
     "MongoDB with index": "green",
     
-    "PostgreSQL text json": "skyblue",
     "PostgreSQL binary json": "deepskyblue",
     "PostgreSQL with index jsonb_ops": "lightskyblue",
     "PostgreSQL with index jsonb_path_ops": "steelblue",
+}
+
+label_names = {
+    "MongoDB": "MDB",
+    "MongoDB with index": "MDB indexed",
+    
+    "PostgreSQL binary json": "PSQL",
+    "PostgreSQL with index jsonb_ops": "PSQL indexed\n(jsonb_ops)",
+    "PostgreSQL with index jsonb_path_ops": "PSQL indexed\n(jsonb_path_ops)",
 }
 
 def normalize(description, names):
@@ -39,26 +47,31 @@ def preprocess(results):
         ncols = width,
         nrows = (len(results) + width - 1) // width,
         # This determines the size in pixels of the final image
-        figsize = [width * 6 * i for i in (1, 1)]
+        figsize = [width * 8 * i for i in (1, 1)]
     )
     
     return width, fig, axes
+
+def make_bar(ax, labels, data):
+    for label, d in zip(labels, data):
+        ax.bar(
+            label_names[label],
+            numpy.average(d) * 1000,
+            yerr = numpy.std(d) * 1000,
+            width = 0.35,
+            color = colors[label],
+        )
 
 def savefigs(output_name, results):
     width, fig, axes = preprocess(results)
     
     for result, index in zip(results.values(), range(len(results))):
         ax = axes[index // width][index % width]
+        
         labels = list(result["columns"].keys())
         data = [column for column in result["columns"].values()]
         
-        bar = ax.bar(
-            labels,
-            [numpy.average(d) * 1000 for d in data],
-            yerr = [numpy.std(d) * 1000 for d in data],
-            width = 0.35,
-            color = [colors[col] for col in labels],
-        )
+        make_bar(ax, labels, data)
         
         ax.set_title(result["description"])
         
@@ -71,6 +84,27 @@ def savefigs(output_name, results):
         
     plt.savefig(output_name)
 
+def savefigs_each(output_prefix, results):
+    for (result_name, result), index in zip(results.items(), range(len(results))):
+        fig, ax = plt.subplots(figsize = (8, 8))
+        
+        labels = list(result["columns"].keys())
+        data = [column for column in result["columns"].values()]
+        
+        make_bar(ax, labels, data)
+        
+        ax.set_title(result["description"])
+        
+        ax.set_ylim(0)
+        
+        ## Set axis names
+        ax.yaxis.grid(True)
+        ax.set_xlabel("Database type")
+        ax.set_ylabel("Time [ms]")
+        
+        plt.savefig(output_prefix + result_name + ".png")
+        plt.close(fig)
+
 def savefigs_grouped(output_name, results):
     width, fig, axes = preprocess(results)
     
@@ -81,7 +115,7 @@ def savefigs_grouped(output_name, results):
         data = [column for column in result["columns"].values()]
         
         bar = ax.bar(
-            labels,
+            [label_names[label] for label in labels],
             [numpy.average(d) for d in data],
             yerr = [numpy.std(d) for d in data],
             width = 0.35,
@@ -104,9 +138,12 @@ def savefigs_grouped(output_name, results):
 with open("results.json", "r") as results_file:
     results = json.load(results_file)
 
+savefigs_each("query-", results)
+
 savefigs("queries.png", results)
 
-savefigs_grouped("queries-grouped.png", [normalize("selecty", [
+savefigs_grouped("queries-grouped.png", [
+    normalize("selecty", [
         "6", "7", "21", "24", "26", "28",
     ]),
     normalize("selecty s podmienkami", [
